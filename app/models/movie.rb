86fbbbca3189e24f1fcaac5e2c65084dc7dcd720
Charles De Barros <charlesdebarros@yahoo.co.uk>
2015-04-19 22:18:03 +0100

    Adding PaperClip migration

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,8 @@
 class Movie < ActiveRecord::Base
 
+has_attached_file :image, styles: { medium: "400x600#", thumb: "100x200>" }, default_url: "/images/:style/missing.png"
+validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
 belongs_to :user
 
 end
