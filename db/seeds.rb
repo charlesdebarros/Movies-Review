5071f0cbadcf20025f9d4ba6871a42682e7024d8
Charles De Barros <charlesdebarros@outlook.com>
2021-03-01 13:32:47 +0000

    Changing Views from ERB to Haml.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,14 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 user_1 = User.create(
-  id: 1,
+  id: 999,
   email: 'user@email.com',
   password: 'Password123',
   password_confirmation: 'Password123'
 )
 
 movie_1 = Movie.create(
-  id: 1,
+  id: 901,
   title: 'Harry Potter and The Order of the Phoenix',
   description: "With their warning about Lord Voldemort's return scoffed at, Harry and Dumbledore are targeted by the Wizard authorities as an authoritarian bureaucrat slowly seizes power at Hogwarts.",
   movie_length: '138',
@@ -24,11 +24,11 @@ movie_1 = Movie.create(
   image_content_type: 'image/jpeg',
   image_file_size: 37495,
   image_updated_at: Time.now,
-  user_id: 1
+  user_id: 999
 )
 
 movie_2 = Movie.create(
-  id: 2,
+  id: 902,
   title: 'Star Wars - Episode IV - New Hope',
   description: "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire's world-destroying battle station, while also attempting to rescue Princess Leia from the evil Darth Vader.",
   movie_length: '121',
@@ -38,11 +38,11 @@ movie_2 = Movie.create(
   image_content_type: 'image/jpeg',
   image_file_size: 39605,
   image_updated_at: Time.now,
-  user_id: 1
+  user_id: 999
 )
 
 movie_3 = Movie.create(
-  id: 3,
+  id: 903,
   title: 'The Silence of the Lamb',
   description: "A young F.B.I. cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer, a madman who skins his victims.",
   movie_length: '118',
@@ -52,11 +52,11 @@ movie_3 = Movie.create(
   image_content_type: 'image/jpeg',
   image_file_size: 176967,
   image_updated_at: Time.now,
-  user_id: 1
+  user_id: 999
 )
 
 movie_4 = Movie.create(
-  id: 4,
+  id: 904,
   title: 'Jaws',
   description: "When a killer shark unleashes chaos on a beach community, it's up to a local sheriff, a marine biologist, and an old seafarer to hunt the beast down.",
   movie_length: '124',
@@ -66,37 +66,37 @@ movie_4 = Movie.create(
   image_content_type: 'image/jpeg',
   image_file_size: 86524,
   image_updated_at: Time.now,
-  user_id: 1
+  user_id: 999
 )
 
 review_1 = Review.create(
-  id: 1,
+  id: 901,
   rating: 4,
   comment: 'Simply superb!',
-  user_id: 1,
-  movie_id: 1
+  user_id: 999,
+  movie_id: 901
 )
 
 review_2 = Review.create(
-  id: 2,
+  id: 902,
   rating: 5,
   comment: 'A Timeless Classic',
-  user_id: 1,
-  movie_id: 2
+  user_id: 999,
+  movie_id: 902
 )
 
 review_3 = Review.create(
-  id: 3,
+  id: 903,
   rating: 4,
   comment: 'Suspense at its best! Really intense and entertaining!',
-  user_id: 1,
-  movie_id: 3
+  user_id: 999,
+  movie_id: 903
 )
 
 review_4 = Review.create(
-  id: 4,
+  id: 904,
   rating: 5,
   comment: 'Incredibly scary. An all time classic!',
-  user_id: 1,
-  movie_id: 4
+  user_id: 999,
+  movie_id: 904
 )
